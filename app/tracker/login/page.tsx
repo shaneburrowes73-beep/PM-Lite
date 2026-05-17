c019c3967f7694b7c676885f0a45a709a290af0c
AI Solutions <aisolutions@aisolutionsnet.net>
2026-05-17 17:05:04 -0400

    Fix: ESLint errors (apostrophe escaping, useEffect deps)

--- app/tracker/login/page.tsx
+++ app/tracker/login/page.tsx
@@ -90,7 +90,7 @@ export default function LoginPage() {
 
         <div className="mt-6 text-center">
           <p className="text-sm text-gray-600">
-            Don't have an account?{' '}
+            Do not have an account?{' '}
             <Link href="/tracker/register" className="text-blue-600 hover:underline font-medium">
               Sign up
             </Link>
